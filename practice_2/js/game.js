02df42f8b07e7f40019bee794f2ceb94e0f8d488
David Brodie <david.brodie122@gmail.com>
2017-11-16 22:05:05 -0500

    begin coding

--- practice_2/js/game.js
+++ practice_2/js/game.js
@@ -1,13 +1,31 @@
 var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
-function preload() {
+var ship;
+var cursors;
 
+function preload() {
+  game.load.atlas('ship', 'assets/red-ship.png', 'assets/red-ship.json');
 };
 
 function create() {
+  ship = game.add.sprite(game.world.centerX, game.world.centerY, 'ship');
+  ship.frame = 1;
+  ship.anchor.setTo(0.5);
+  ship.animations.add('fly', Phaser.Animation.generateFrameNames('sprite', 1, 3));
+  game.physics.arcade.enable(ship);
+
+  cursors = this.input.keyboard.createCursorKeys();
 
 };
 
 function update() {
-
+  ship.animations.play('fly', 5, true);
 };
+
+function handleShipMovement() {
+  if (cursors.up.isDown) {
+    game.physics.arcade.accelerationFromRotation(sprite.rotation, 300, sprite.body.acceleration);
+  }
+};
+
+
